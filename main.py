e86518779410c2654b6ae4e70c1de84a29dfa3c1
Oliver Then <oli171dr@gmail.com>
2023-10-06 13:14:53 -0400

    OpenweatherApi First Test

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
+key_api = '7bde4586e85a32fd9b24b872e8506e30'
+
 @app.get("/")
-async def root():
-	return {"message": "Weather App"}
+def root():
+	lat = input("Enter latitude: ")
+	lon = input("Enter longitude: ")
+
+	url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={key_api}'
+
+	response = requests.get(url)
+	data = response.json()
+
+	# if response.status_code == 200:
+	# 	data = response.json()
+	# 	# temp = data['current']['temp']
+	# 	# desc = data['curent']['weather'][descrip tion]
+	# else:
+	# 	print('Error fetching weather data')
+
+	return data['current']['temp']
+
+# @app.get("/{city_id}")
+# async def root():
+# 	return {"message": "Weather App"}
