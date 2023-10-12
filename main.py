444ca77f3140182bdd2e0947167a3db529d96377
Oliver Then <oli171dr@gmail.com>
2023-10-12 07:55:22 -0400

    Initial app done

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
 from decouple import config
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import requests
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 key_api = config('API_KEY')
 
-@app.get("/")
-def root():
-	lat = input("Enter latitude: ")
-	lon = input("Enter longitude: ")
+@app.get("/", response_class=HTMLResponse)
+async def get_coordinates(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-	url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={key_api}'
 
-	response = requests.get(url)
-	data = response.json()
+@app.post("/process_coordinates", response_class=HTMLResponse)
+async def process_coordinates(
+    request: Request,
+    latitude: float = Form(...),
+    longitude: float = Form(...)
+):
+    
+    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={str(latitude)}&lon={str(longitude)}&appid={key_api}'
 
-	# if response.status_code == 200:
-	# 	data = response.json()
-	# 	# temp = data['current']['temp']
-	# 	# desc = data['curent']['weather'][descrip tion]
-	# else:
-	# 	print('Error fetching weather data')
+    response = requests.get(url)
+    data = response.json()
+    
+    temp_c = data['current']['temp'] - 273.15
+    temp_f = (temp_c * 9/5) + 32
+    print("Temp in C is", temp_c)
 
-	return data['current']['temp']
-
-# @app.get("/{city_id}")
-# async def root():
-# 	return {"message": "Weather App"}
+    return templates.TemplateResponse("weather.html", {"request": request, "temp_c": round(temp_c), "temp_f": round(temp_f)})
