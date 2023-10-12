f442783c10a4282048149003baa243436ce94163
Oliver Then <oli171dr@gmail.com>
2023-10-12 13:33:56 -0400

    Finished Initial App

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ templates = Jinja2Templates(directory="templates")
 
 key_api = config('API_KEY')
 
+def convert_celsius_to_fahrenheight(temp_celsius: float) -> float:
+	return (temp_celsius * 9/5) + 32
+
+def convert_kelvin_to_celsius(temp_kelvin: float) -> float:
+    return temp_kelvin - 273.15
+
 @app.get("/", response_class=HTMLResponse)
 async def get_coordinates(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -22,13 +28,15 @@ async def process_coordinates(
     longitude: float = Form(...)
 ):
     
-    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={str(latitude)}&lon={str(longitude)}&appid={key_api}'
+    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={key_api}'
 
     response = requests.get(url)
     data = response.json()
     
-    temp_c = data['current']['temp'] - 273.15
-    temp_f = (temp_c * 9/5) + 32
-    print("Temp in C is", temp_c)
+    temp_c = convert_kelvin_to_celsius(data['current']['temp'])
+
+    temp_f = convert_celsius_to_fahrenheight(temp_c)
+
 
     return templates.TemplateResponse("weather.html", {"request": request, "temp_c": round(temp_c), "temp_f": round(temp_f)})
+
