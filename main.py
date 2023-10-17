f2f60fcca25df7f1506e5afe50806d8a05db02c3
Oliver Then <oli171dr@gmail.com>
2023-10-17 07:31:10 -0400

    Integrated models with app

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import Depends, HTTPException, FastAPI, Form, Request
 from decouple import config
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse 
+from sqlmodel import Session, select
+
+from .models import User, get_session, create_db_and_tables
+
 import requests
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+	create_db_and_tables()
+	
 
 templates = Jinja2Templates(directory="templates")
 
@@ -14,29 +23,43 @@ def convert_celsius_to_fahrenheight(temp_celsius: float) -> float:
 	return (temp_celsius * 9/5) + 32
 
 def convert_kelvin_to_celsius(temp_kelvin: float) -> float:
-    return temp_kelvin - 273.15
+	return temp_kelvin - 273.15
+
+
 
 @app.get("/", response_class=HTMLResponse)
 async def get_coordinates(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+	return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/process_coordinates", response_class=HTMLResponse)
 async def process_coordinates(
-    request: Request,
-    latitude: float = Form(...),
-    longitude: float = Form(...)
+	request: Request,
+	latitude: float = Form(...),
+	longitude: float = Form(...)
 ):
-    
-    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={key_api}'
+	
+	url = f'https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={key_api}'
 
-    response = requests.get(url)
-    data = response.json()
-    
-    temp_c = convert_kelvin_to_celsius(data['current']['temp'])
+	response = requests.get(url)
+	data = response.json()
+	
+	temp_c = convert_kelvin_to_celsius(data['current']['temp'])
 
-    temp_f = convert_celsius_to_fahrenheight(temp_c)
+	temp_f = convert_celsius_to_fahrenheight(temp_c)
 
 
-    return templates.TemplateResponse("weather.html", {"request": request, "temp_c": round(temp_c), "temp_f": round(temp_f)})
+	return templates.TemplateResponse("weather.html", {"request": request, "temp_c": round(temp_c), "temp_f": round(temp_f)})
+
+@app.post("/register")
+def register_user(session: Depends(get_session), user: User):
+	query = select(User).where(User.handle == user.handle)
+	existing_user = session.exec(query).first()
+	if existing_user is not None:
+		raise HTTPException(status_code=400, detail="User already exists")
+	db_user = User.from_orm(user)
+	session.add(db_user)
+	session.commit()
+	session.refresh(db_user)
+	return db_user
 
--- models.py
+++ models.py
@@ -2,12 +2,31 @@ from typing import Optional
 
 from sqlmodel import Field, SQLModel, Session, create_engine
 
-class User(SQLModel, table=True):
-	id: Optional[int] = Field(default=None, primary_key=True)
+class UserBase(SQLModel):
 	first_name: str
 	last_name: str
+	username: str
 	email: str
 	zip_code: int
 	password: str
 
-engine = create_engine("sqlite:///database.db")
+class User(UserBase, table=True):
+	id: Optional[int] = Field(default=None, primary_key=True)
+	
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+def get_session():
+	with Session(engine) as session:
+		yield session
+
+def create_db_and_tables():
+	SQLModel.metadata.create_all(engine)
+
+# def create_user(db: Session, user: User):
+# 	db.add(user)
+# 	db.commit()
+# 	db.refresh(user)
